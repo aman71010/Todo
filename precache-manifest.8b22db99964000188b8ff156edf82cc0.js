ff0e7bd51b846a4814e5f6c7c417aa1d0559c69a
Aman <amangupta71010@gmail.com>
2021-07-26 21:38:06 +0530

    Updates

---
+++ precache-manifest.8b22db99964000188b8ff156edf82cc0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ec31212506917c12222103f2c65880",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a850c488d5451483c9e4",
+    "url": "/todo/static/js/2.6fc1816f.chunk.js"
+  },
+  {
+    "revision": "12cd65c9693a4c918251",
+    "url": "/todo/static/js/main.ba06b58c.chunk.js"
+  },
+  {
+    "revision": "c3f644d44a354b689fcd",
+    "url": "/todo/static/js/runtime-main.2c5425c6.js"
+  }
+]);
