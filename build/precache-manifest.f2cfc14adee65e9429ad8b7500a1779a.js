abd305f8308c0b8c5d4c027799fd672582975e58
Aman <amangupta71010@gmail.com>
2021-07-27 00:11:39 +0530

    deploy app to github

---
+++ build/precache-manifest.f2cfc14adee65e9429ad8b7500a1779a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8755af44670c05143ce2f2f049ef631",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "a850c488d5451483c9e4",
+    "url": "/Todo/static/js/2.6fc1816f.chunk.js"
+  },
+  {
+    "revision": "12cd65c9693a4c918251",
+    "url": "/Todo/static/js/main.ba06b58c.chunk.js"
+  },
+  {
+    "revision": "bc183832d29d39c8aeff",
+    "url": "/Todo/static/js/runtime-main.f1d35d2b.js"
+  }
+]);
